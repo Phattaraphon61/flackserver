5e2206fc78b6ecbb74c360988d156180e6dc9e99
Phattaraphon Pewruang <phattaraphon.pe.61@ubu.ac.th>
2021-01-05 01:07:51 +0700

    uploadanddownloadfile

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,27 @@
 
-# A very simple Flask Hello World app for you to get started with...
+from flask import Flask, render_template, request,send_file
+from werkzeug.utils import secure_filename
+import os
 
-from flask import Flask
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask!7777'
+    return 'Hello from Flask!888'
 
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      filename = secure_filename(f.filename)
+      f.save(os.path.join("files/",filename))
+    #   f.save(secure_filename("files/"+f.filename))
+      return 'file uploaded successfully'
+@app.route('/download/<string:name>')
+def downloadFile(name):
+    return send_file("files/"+name, as_attachment=True)
+if __name__ == '__main__':
+   app.run(debug = True)
